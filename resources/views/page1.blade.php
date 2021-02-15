a71251158eef35339b3313dbeace5c85c837465e
Shipix <shipix@hotmail.fr>
2021-02-15 12:26:24 +0100

    PT-1

---
+++ resources/views/page1.blade.php
@@ -0,0 +1,8 @@
+@extends('template.main')
+@section('content')
+@include('partial.header')
+    <h1>Rayane</h1>
+    <h2>Ben</h2>
+    <h3>Mon age est de 25 ans</h3>
+    <h4>mon e-mail shipix@hotmail.fr</h4>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+@section('content')
+@include('partial.header')
+    <h1>Bienvenue</h1>
+@endsection
